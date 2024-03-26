1ff81570da642b86a7c3a00b00f4e7b3c6292eff
Alex <alexyufeizeng@gmail.com>
2024-03-25 21:16:11 -0600

    configuration works from website to firebase

--- web_interface/main.py
+++ web_interface/main.py
@@ -1,14 +1,20 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from google.cloud import firestore
 import firebase_admin
 from firebase_admin import credentials
 from google.cloud import storage
 
-
 # Flask
-###########################################################################################
 app = Flask(__name__)
+
+# Initialize Firebase
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/Alex/Documents/AMDT/image_capture/DSLR/amdt-a1b24-firebase-adminsdk-q32q3-c9217e3b84.json"
+default_app = firebase_admin.initialize_app()
+db = firestore.Client()
+
+# Firestore document reference
+doc_ref = db.collection("sysConfig").document("HvAny5Q1B26cU8PNC1lA")
 
 # Routes
 @app.route('/')
@@ -38,18 +44,53 @@ def diagnostics():
 
 @app.route('/configure')
 def configure():
-    return render_template('configure.html')
-###########################################################################################
+    start_date, start_time, end_date, end_time = read_sys_config()
+    return render_template('configure.html', start_date=start_date, start_time=start_time, end_date=end_date, end_time=end_time)
+    #return render_template('configure.html')
+                           
+# Flask route for handling form submission
+@app.route('/update_run', methods=['POST'])
+def update_run():
+    if request.method == 'POST':
+        data = request.json
+        start_time = data['startTime']
+        end_time = data['endTime']
+        start_day = data['startDay']
+        end_day = data['endDay']
+
+        # Reference to the document
+        doc_ref = db.collection("sysConfig").document("HvAny5Q1B26cU8PNC1lA")
+
+        # Update Firestore document with start and end times
+        doc_ref.update({
+            'StartTime': start_time,
+            'EndTime': end_time,
+            'StartDate': start_day,
+            'EndDate': end_day
+        })
+        
 
 
-
-# TODO Change so key is not hardcoded
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/Alex/Documents/AMDT/image_capture/DSLR/amdt-a1b24-firebase-adminsdk-q32q3-c9217e3b84.json"
-
-# Initialize Firebase
-default_app = firebase_admin.initialize_app()
-db = firestore.Client()
-
+def read_sys_config():
+    # Reference to the document
+    doc_ref = db.collection("sysConfig").document("HvAny5Q1B26cU8PNC1lA")
+    
+    # Retrieve the document snapshot
+    doc = doc_ref.get()
+    
+    if doc.exists:
+        # Get data from the document
+        data = doc.to_dict()
+        
+        end_date_str = data.get("EndDate")
+        end_time_str = data.get("EndTime")
+        start_date_str = data.get("StartDate")
+        start_time_str = data.get("StartTime")   
+        
+        return start_date_str, start_time_str, end_date_str, end_time_str
+    else:
+        print("Document does not exist")
+        return None, None, None, None
 
 
 def get_image_data():
@@ -77,16 +118,9 @@ def get_image_data():
             image_data.append({'url': image_url, 'date': creation_time})
 
     return image_data
-  
-    
 
-if __name__ == '__main__':  
-    
-    #Run Flask
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-    
-    
-    
-    
-    
-    
